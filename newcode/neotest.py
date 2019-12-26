52c8afb6a9941adbc26d9e3d7b4c50d62d87f325
Joseph <78599297@qq.com>
2019-12-26 09:11:38 +0800

    test

--- newcode/neotest.py
+++ newcode/neotest.py
@@ -1,13 +1,19 @@
 # coding:utf-8
-from py2neo import Graph, Node, Relationship, RelationshipMatcher
+from py2neo import Graph, Node, Relationship, RelationshipMatcher,NodeMatcher
 
 graph = Graph("bolt://127.0.0.1:7687", username='neo4j', password='123456' )
 
 
 
-a = Node('Person', name='fengling')
-b = Node('Person', name='yingying')
-graph.create(a)
-graph.create(b)
-r = Relationship(a, 'KNOWS', b)
-print(a, b , r)
+# a = Node('Person', name='fengling')
+# b = Node('Person', name='yingying')
+# graph.create(a)
+# graph.create(b)
+# r = Relationship(a, 'KNOWS', b)
+# print(a, b , r)
+
+# print(graph.run("MATCH (a:Person) RETURN a.name").data()[2])
+matcher=NodeMatcher(graph)
+
+people=len(matcher.match(name="fengling"))
+print(people)
